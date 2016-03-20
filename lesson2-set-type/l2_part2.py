1db85e52591213716dc7a39fed0c2493fc461277
yprodev <yprodanchuk@gmail.com>
2016-03-20 13:11:31 +0200

    Additional methods for sets

---
+++ lesson2-set-type/l2_part2.py
@@ -0,0 +1,18 @@
+# Additional Methods
+# ==================================================
+
+'''
+	.clear
+	.difference
+	.difference_update
+	.intersection
+	.intersection_update
+	.symmetric_difference
+	.symmetric_difference_update
+
+'''
+
+# ==================================================
+# Let's take a look at the clear method
+animals = {'dog', 'cat', 'bird'}
+print(animals)
