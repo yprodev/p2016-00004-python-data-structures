caffc203116e428a8fa857d69dd7fa7682de3f6c
yprodev <yprodanchuk@gmail.com>
2016-03-27 18:35:12 +0300

    Common operations with tuples and lists

---
+++ lesson3-list-and-tuple/l3_part12.py
@@ -0,0 +1,12 @@
+# Common methods - .Count And .Index
+# ==================================================
+
+fruits = ['apple', 'banana', 'apple', 'pear', 'strawberry']
+print(fruits.count('apple')) # 2
+
+profile = 'Ford', 'Madox', 'Ford'
+print(profile.count('Ford')) # 2
+
+
+print(fruits.index('pear')) # 3
+print(profile.index('Madox')) # 1
